d7b2f290e26a997fc91df297a90d9742671a1b10
Nate Michalov <nmichalov@endor.ai>
2024-08-06 13:41:42 -0400

    try that one

--- src/main/java/com/endor/stringsub.java
+++ src/main/java/com/endor/stringsub.java
@@ -3,8 +3,9 @@ import org.apache.commons.text.StringSubstitutor;
 
 public class stringsub {
     public static void main(String... args) {
-        // final StringSubstitutor interpolator = StringSubstitutor.createInterpolator();
+        final StringSubstitutor interpolator = StringSubstitutor.createInterpolator();
         // String out = interpolator.replace("${script:javascript:java.lang.Runtime.getRuntime().exec('touch ./foo')}");
-        System.out.println('yeah');
+        String out = "bababooy";
+        System.out.println(out);
     }    
 }
